c02b032175e55ce4de84496fccfce0a0761fff03
Mikołaj Zagojski <mikolajzagojski@gmail.com>
2026-09-13 22:00:14 +0200

    feat(ingestion): add MinIO client with bucket check and JSON upload

---
+++ ingestion/minio_uploader.py
@@ -0,0 +1,45 @@
+import os 
+import io
+import json
+from dotenv import load_dotenv
+from minio import Minio
+from minio.error import S3Error
+load_dotenv()
+
+def get_client() -> Minio:
+    client = Minio(
+        os.getenv('MINIO_ENDPOINT'),
+        access_key = os.getenv('MINIO_ROOT_USER'),
+        secret_key = os.getenv('MINIO_ROOT_PASSWORD'),
+        secure = os.getenv('MINIO_USE_SSL', 'false').lower() == 'true')
+
+    return client
+
+def upload_json_to_minio(client: Minio, bucket_name: str, object_name: str, payload: dict):
+
+    body = json.dumps(payload,ensure_ascii=False).encode('utf-8')
+    client.put_object(
+        bucket_name,
+        object_name,
+        data = io.BytesIO(body),
+        length = len(body),
+        content_type='application/json'
+   )
+
+def ensure_bucket_exists(client: Minio, bucket_name: str):
+    if not client.bucket_exists(bucket_name):
+        client.make_bucket(bucket_name)
+        print(f'Created bucket {bucket_name}')
+
+if __name__ == '__main__':
+    try: 
+        minio_client = get_client()
+        bucket_name = 'football-data'
+        object_name = 'test.json'
+        payload = {'key': 'value'}
+
+        ensure_bucket_exists(minio_client,bucket_name)
+        upload_json_to_minio(minio_client, bucket_name, object_name, payload)
+        print(f'Successfully uploaded {object_name} to bucket {bucket_name}.')
+    except S3Error as exc:
+        print(f'Error occurred {exc}')
